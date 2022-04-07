6daf3be6443d92112405c98ba376ade2d3aafb45
João Marquês <99401638+j-b-marques@users.noreply.github.com>
2022-04-07 15:54:02 +0100

    Comit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//******** CLOSE MOBILE MENU **********/
+
+const openMenu = document.querySelector('.burguer__menu');
+const mobileMenu = document.querySelector('.nav__mobile');
+
+openMenu.addEventListener('click', () => {
+  mobileMenu.style.top = '50%';
+});
+
+const closeMenu = document.querySelector('.close__menu');
+const mobileMenuClose = document.querySelector('.nav__mobile');
+
+closeMenu.addEventListener('click', () => {
+  mobileMenuClose.style.top = '150%';
+});
